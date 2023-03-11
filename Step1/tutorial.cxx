f5857b96bbcf0331d7fa5f7ba89d5e057f0db31e
Tapti Palit <tpalit@purdue.edu>
2023-03-11 17:18:33 +0000

    Setting up a project. How to initialize variables

--- Step1/tutorial.cxx
+++ Step1/tutorial.cxx
@@ -1,10 +1,11 @@
 // A simple program that computes the square root of a number
 #include <cmath>
-#include <cstdlib> // TODO 5: Remove this line
+//#include <cstdlib> // TODO 5: Remove this line
 #include <iostream>
 #include <string>
 
 // TODO 11: Include TutorialConfig.h
+#include <TutorialConfig.h>
 
 int main(int argc, char* argv[])
 {
@@ -15,9 +16,10 @@ int main(int argc, char* argv[])
     return 1;
   }
 
+  std::cout << "Version: " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << "\n";
   // convert input to double
   // TODO 4: Replace atof(argv[1]) with std::stod(argv[1])
-  const double inputValue = atof(argv[1]);
+  const double inputValue = std::stod(argv[1]);
 
   // calculate square root
   const double outputValue = sqrt(inputValue);
